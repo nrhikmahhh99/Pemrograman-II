f3f922a07ac6aa6c8a490a39869c1ad9d011f1d7
nrhikmahhh99 <144782363+nrhikmahhh99@users.noreply.github.com>
2024-12-18 16:11:14 +0800

    Add files via upload

---
+++ PRAK501_2310817120010_NurHikmah/src/soal1/Rectangle.java
@@ -0,0 +1,19 @@
+package soal1;
+
+public class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle (double l, double w){
+        super("Rectangle");
+        this.length=l;
+        this.width=w;
+    }
+
+    public double area(){
+        return length*width;
+    }
+
+    public String toString(){
+        return super.toString() + " of length " + length + " and width " + width;
+    }
+}
